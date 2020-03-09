90abea9829ba38b222bf5b2a969e9d3c18317327
miccoli-alessia <miccoli.alessia@hotmail.it>
2020-03-09 14:14:47 +0100

    Initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'Hello'
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,3 @@
+var template = <p>Hello</p>
+var appRoot= document.getElementById('app');
+ReactDOM.render(template, appRoot);
