c1c1e56af3a7e00d45cb9939abdbfd3321b1af88
miccoli-alessia <miccoli.alessia@hotmail.it>
2020-03-16 20:47:20 +0100

    refactorin of functional components

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default function Action(props) {
-  return (
-    <div>
-      <button
-        disabled={!props.hasOptions}
-        onClick={props.handlePick}>What should I do?</button>
-    </div>
-  );
-}
+const Action = (props) => (
+  <div>
+    <button
+      disabled={!props.hasOptions}
+      onClick={props.handlePick}
+      >
+      What should I do?
+      </button>
+  </div>
+);
+
+export default Action;
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Option(props) {
-  return (
-    <div>
-      <p>{props.optionText}</p>
-      <button onClick={() => props.handleDeleteOption(props.optionText)}>Remove</button>
-    </div>
-  );
-}
+const Option = (props) => (
+  <div>
+    <p>{props.optionText}</p>
+    <button onClick={() => props.handleDeleteOption(props.optionText)}>Remove</button>
+  </div>
+);
+
+export default Option;
